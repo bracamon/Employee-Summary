9c2ffa4f71fedc498414f98aadb58042c02167c8
bracamon <bracamontes.ochoa@gmail.com>
2020-12-07 21:40:51 -0800

    added inquirer prompts to Employee.js

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,5 +1,12 @@
 // TODO: Write code to define and export the Employee class
+const inquirer = require("inquirer");
 
+inquirer
+    .prompt([
+        {type: "input", name: "name", message: "enter name"},
+        {type: "input", name: "id", message: "enter ID"},
+        {type: "input", name: "email", message: "enter Email"}
+    ])
 
 
 module.exports = employee;
