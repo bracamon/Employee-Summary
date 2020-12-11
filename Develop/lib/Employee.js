7d56dedb308192707dac736f0b1885c4149cc12e
bracamon <bracamontes.ochoa@gmail.com>
2020-12-10 22:10:23 -0800

    created class for engineer that extends employee class

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -21,6 +21,4 @@ class Employee {
     };
 };
 
-
-
 module.exports = Employee;
--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,15 +1,17 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+const Employee = require("./Employee");
 
-// const Employee = require("./Employee");
-const inquirer = require("inquirer");
-
-const Engineer = function() { 
-    inquirer
-        .prompt([
-            {type: "input", name: "githubInput", message: "enter GitHub username"}
-        ]);
+class Engineer extends Employee {
+    constructor(name, id, email, github) {
+        super(name, id, email)
+        this.github = github;
+    };
+    getGithub() {
+        return this.github;
+    };
+    getRole() {
+        return "Engineer";
+    };
 };
-
-Engineer();
 
 module.exports = Engineer;
