53458967d3f6ccca6765eb5fac9e7043f410baf2
bracamon <bracamontes.ochoa@gmail.com>
2020-12-10 18:22:35 -0800

    returning name, id, and email - Employee Test passes

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,55 +1,26 @@
 // TODO: Write code to define and export the Employee class
-// const Employee = require("./Employee");
 const inquirer = require("inquirer");
 
 class Employee {
-    constructor(name) {
+    constructor(name, id, email) {
         this.name = name;
+        this.id = id;
+        this.email = email;
     };
-    // constructor(id) {
-    //     this.id = id;
-    // };
-    // constructor(email) {
-    //     this.email = email;
-    // };
-    introduce() {
-        console.log(`Hello, my name is ${this.name}`);
+    getName() {
+        return this.name;
+    };
+    getId() {
+        return this.id;
+    };
+    getEmail() {
+        return this.email;
+    };
+    getRole() {
+        return "Employee";
     };
 };
 
-const otto = new Employee(`Otto`);
 
-otto.introduce();
-
-// const Employee = function() { 
-//     inquirer
-//     .prompt([
-//         {type: "input", name: "name", message: "enter name"},
-//         {type: "input", name: "id", message: "enter ID"},
-//         {type: "input", name: "email", message: "enter Email"},
-//         {type: "list", name: "roleInput", message: "select role", choices: [
-//             "engineer",
-//             "manager",
-//             "intern",
-//             ],
-//         },
-//     ]);
-// };
-
-
-// inquirer
-//     .prompt([
-//         {type: "input", name: "nameInput", message: "enter name"},
-//         {type: "input", name: "idInput", message: "enter ID"},
-//         {type: "input", name: "emailInput", message: "enter Email"},
-//         {type: "list", name: "roleInput", message: "select role", choices: [
-//             "engineer",
-//             "manager",
-//             "intern",
-//             ],
-//         },
-//     ])
-
-// Employee();
 
 module.exports = Employee;
